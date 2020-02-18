8f76a7d95ce8943c8f77a7cdaa098ed6097cf0a6
Vicki Thomson <vicki.thomson@adelaide.edu.au>
2020-02-18 18:09:52 +1030

    Loading libraries

--- Analysis.R
+++ Analysis.R
@@ -4,4 +4,8 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install("Rsamtools", version = "3.9")
 library(Rsamtools)
 
+install.packages("geomorph")
+library(geomorph)
+install.packages("devtools")
+devtools::install_github("cardiomoon/ggiraphExtra")
 
